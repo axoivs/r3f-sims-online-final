c095395a2978ac8e0ecfeb8436b32ac519f93979
axoivs <axoivs1@gmail.com>
2025-03-28 04:21:58 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -93,7 +93,7 @@ loadRooms();
 
 const generateRandomPosition = (room) => {
   // TO AVOID INFINITE LOOP WE LIMIT TO 100, BEST WOULD BE TO CHECK IF THERE IS ENOUGH SPACE LEFT 🤭
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 1000; i++) {
     const x = Math.floor(Math.random() * room.size[0] * room.gridDivision);
     const y = Math.floor(Math.random() * room.size[1] * room.gridDivision);
     if (room.grid.isWalkableAt(x, y)) {
