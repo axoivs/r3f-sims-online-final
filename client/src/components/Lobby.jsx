771a4837bcfbe053fefe4b84fed219c1f3b134c4
axoivs <axoivs1@gmail.com>
2025-03-29 04:33:47 -0400

    Update Lobby.jsx

--- client/src/components/Lobby.jsx
+++ client/src/components/Lobby.jsx
@@ -144,7 +144,7 @@ export const Lobby = () => {
           bevelThickness={0.005}
           letterSpacing={0.012}
         >
-          Welcome
+          Join
           <meshStandardMaterial color="#90EE90" />
         </Text3D>
 
@@ -159,7 +159,7 @@ export const Lobby = () => {
           bevelThickness={0.005}
           letterSpacing={0.012}
         >
-          Crazies
+          Forum!
           <meshStandardMaterial color="#90EE90" />
         </Text3D>
         <Skyscraper scale={1.32} />
