2424496c2713cff5a89858edb0a799a0dcca11fb
axoivs <axoivs1@gmail.com>
2025-03-29 06:16:43 -0400

    Update Lobby.jsx

--- client/src/components/Lobby.jsx
+++ client/src/components/Lobby.jsx
@@ -137,7 +137,7 @@ export const Lobby = () => {
           font={"fonts/Inter_Bold.json"}
           position-z={1}
           size={0.3}
-          position-x={-2.4}
+          position-x={-3.4}
           castShadow
           rotation-y={Math.PI / 8}
           bevelEnabled
